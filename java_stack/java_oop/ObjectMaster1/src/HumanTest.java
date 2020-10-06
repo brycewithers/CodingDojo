910d8e57bc26142ac8147b162aa6942675f1b743
Bryce <bryce.everett.withers@gmail.com>
2020-10-05 21:37:09 -0700

    adding ObjectMaster1

---
+++ java_stack/java_oop/ObjectMaster1/src/HumanTest.java
@@ -0,0 +1,12 @@
+
+public class HumanTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Human bob = new Human();
+		Human rob = new Human();
+		bob.attacked(rob);
+		rob.displayHealth();
+	}
+
+}
