20ffac2346a84ac2c7d8f91ee3990655a4df6c39
Bryce <bryce.everett.withers@gmail.com>
2020-10-07 13:31:34 -0700

    adding GettingFamiliarWithRouting assignment

---
+++ java_stack/spring_fun/GettingFamiliarWithRouting/src/test/java/com/bryce/gettingfamiliarwithrouting/GettingFamiliarWithRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryce.gettingfamiliarwithrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingFamiliarWithRoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
