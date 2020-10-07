72bbe8bc2cb60d71ec851767dc30dd11bc471be4
Bryce <bryce.everett.withers@gmail.com>
2020-10-06 21:32:02 -0700

    adding the new folder Spring_Fun with it's first assignment

---
+++ java_stack/spring_fun/strings/src/main/java/com/bryce/strings/StringsApplication.java
@@ -0,0 +1,26 @@
+package com.bryce.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello client! How are you doing?";
+	}
+	
+	@RequestMapping("/random")
+	public String springBootRocks() {
+		return "With Spring Boot, the Java stack, and graphic design, I will get a great job!";
+	}
+}
